926948a8cff157076dab9e5483c84d7130ba129a
Maximilian Konter <maximiliankonter@aviatocodes.com>
2018-09-27 21:11:14 +0200

    Improve lock

--- .config/scripts/i3lock.sh
+++ .config/scripts/i3lock.sh
@@ -6,6 +6,6 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" -scale 20% -scale 500% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg"
+i3lock --radius 130 -i "$tmpbg"
